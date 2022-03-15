da9732d599ebdefd8dfc52d88ef3f5e21b4f39dd
Bruna <brunatpicoli@outlook.com>
2022-03-15 16:35:15 -0300

    iniciei o trabalho no footer

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst Home = ()=>{\n    return(/*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: \"L2 Code\"\n    }, void 0, false, {\n        fileName: \"D:\\\\estagio-devweb-l2code\\\\site-l2\\\\pages\\\\index.tsx\",\n        lineNumber: 5,\n        columnNumber: 5\n    }, undefined));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC50c3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUVBLEtBQUssQ0FBQ0EsSUFBSSxPQUFtQixDQUFDO0lBQzVCLE1BQU0sNkVBQ0hDLENBQUc7a0JBQUMsQ0FFTDs7Ozs7O0FBRUosQ0FBQztBQUVELGlFQUFlRCxJQUFJIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbm92by1zaXRlLWwyLWZyb250Ly4vcGFnZXMvaW5kZXgudHN4PzA3ZmYiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHR5cGUgeyBOZXh0UGFnZSB9IGZyb20gJ25leHQnXHJcblxyXG5jb25zdCBIb21lOiBOZXh0UGFnZSA9ICgpID0+IHtcclxuICByZXR1cm4gKFxyXG4gICAgPGRpdj5cclxuICAgICAgTDIgQ29kZVxyXG4gICAgPC9kaXY+XHJcbiAgKVxyXG59XHJcblxyXG5leHBvcnQgZGVmYXVsdCBIb21lXHJcbiJdLCJuYW1lcyI6WyJIb21lIiwiZGl2Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.tsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.tsx"));
+module.exports = __webpack_exports__;
+
+})();
--- components/organisms/footer/index.tsx
+++ components/organisms/footer/index.tsx
@@ -1,7 +1,44 @@
-import { FooterStyled } from './styled';
+import { FooterStyled, ContactStyled } from './styled';
+import logo from '../../../assets/images/Logo-Small.svg';
+import heart from '../../../assets/images/Vector.svg';
 
 const Footer = () => {
-  return <FooterStyled>Footer</FooterStyled>;
+  return (
+    <FooterStyled>
+      <section>
+        <h2>VAMOS CONVERSAR</h2>
+
+        <ContactStyled>
+          <h3>nosso whatsapp</h3>
+          <p>+55 51 99693.9336</p>
+        </ContactStyled>
+
+        <ContactStyled>
+          <h3>nosso e-mail</h3>
+          <p>contato@l2code.com.br</p>
+        </ContactStyled>
+
+        <ContactStyled>
+          <h3>entre no time</h3>
+          <p>talentos@l2code.com.br</p>
+        </ContactStyled>
+      </section>
+      <aside>
+        <p>&lt;AND DANCE&gt;</p>
+      </aside>
+      <footer>
+        <section>
+          <p>Abraçamos os talentos do mundo</p>
+          <p>Somos uma empresa que trabalha 100% remoto</p>
+        </section>
+        <img src={logo} alt="L2 Code" />
+        <section>
+          <p>Feito com <img src={heart} alt="Coração" /> L2code</p>
+          <p>Copyright © Todos os direitos reservados</p>
+        </section>
+      </footer>
+    </FooterStyled>
+  );
 };
 
 export default Footer;
