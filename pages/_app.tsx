8eedc77a21692b42adcfd3e8773bf02b830ce806
Bren-Dev <brenda_nogueira_pe@hotmail.com>
2022-03-28 13:58:43 -0300

    Ajeitando erro kkk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,11 @@ import type { AppProps } from 'next/app';
 import Footer from '../components/organisms/footer';
 import Header from '../components/organisms/header/index';
 import OurServices from '../components/organisms/ourServices';
-import OurServicesOnePage from '../components/organisms/ourServicesOnePage';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    
     <>
-      <GlobalStyle />
-      <Header />
-      <OurServices/>
-      <OurServicesOnePage/>
-      <Component {...pageProps} />
-      <Footer />
+      <OurServices />
     </>
   );
 }
