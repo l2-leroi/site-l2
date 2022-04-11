0542a97f41c4abdb88f9b6a289ad25d09e8494d8
Bruna <brunatpicoli@outlook.com>
2022-04-11 17:39:49 -0300

    colocando a chamada do componente no index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import '../styles/fonts.css';
 import type { AppProps } from 'next/app';
 import Footer from '../components/organisms/footer';
 import Header from '../components/organisms/header/index';
-import Cases from '../components/organisms/cases';
 import OurServicesOnePage from '../components/organisms/ourServicesOnePage';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -12,9 +11,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <GlobalStyle />
       <Header />
       <OurServicesOnePage/>
-      <Cases />
       <Component {...pageProps} />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
