21cbd911e9c1f6af8556f3adf4ab2d7f8d75d6af
Bren-Dev <brenda_nogueira_pe@hotmail.com>
2022-04-20 16:17:09 -0300

    adjusts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import type { NextPage } from 'next';
-import OurServices from '../components/organisms/ourServices';
 import Cases from '../components/organisms/cases';
 
 
 const Home: NextPage = () => {
   return (
     <>
-
-      <OurServices />
-
       <Cases />
-
     </>
   );
 };
