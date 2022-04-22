6e9c24c2f973b04ecd046240f5ee8b3e9181e251
Bruna <brunatpicoli@outlook.com>
2022-04-22 17:29:11 -0300

    ajustes de background e responsividade

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 import Cases from '../components/organisms/cases';
+import OurServicesOnePage from '../components/organisms/ourServicesOnePage';
 
 
 const Home: NextPage = () => {
   return (
     <>
+      <OurServicesOnePage />
       <Cases />
     </>
   );
