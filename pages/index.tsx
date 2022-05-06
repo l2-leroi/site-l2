58700b4bbd28fe333a3a105950158b44157093ef
Bruna <brunatpicoli@outlook.com>
2022-05-06 14:25:34 -0300

    alteração no refresh da home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,20 @@ import type { NextPage } from 'next';
 import Cases from '../components/organisms/Cases';
 import OurServicesOnePage from '../components/organisms/OurServicesOnePage';
 import OurCustomers from '../components/organisms/OurCustomers';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+  // make page refresh on top
+  useEffect(() => {
+    if (history.scrollRestoration) {
+      history.scrollRestoration = 'manual';
+    } else {
+      window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+      }
+    }
+  });
+
   return (
     <>
       <OurServicesOnePage />
