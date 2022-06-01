4bd7925ffd6b06ae8f4e26451131c769e35038a0
HeloiseKatharine <katharineheloise@gmail.com>
2022-06-01 18:10:45 -0300

    Update

--- src/pages/[lang]/AllCases/index.tsx
+++ src/pages/[lang]/AllCases/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import { useEffect } from 'react';
 import AllCasesPage from '../../../components/organisms/AllCasesPage';
 import Menu from '../../../components/organisms/Menu';
-import AllMenu from '../../../components/organisms/AllMenu';
 
 const AllCases: NextPage = () => {
   // make page refresh on top
@@ -18,7 +17,6 @@ const AllCases: NextPage = () => {
 
   return (
     <>
-      {/*<AllMenu />*/}
       {/*<Menu openMenu={true} closeMenu = {() => (false)}/>*/}
       <AllCasesPage />
     </>
