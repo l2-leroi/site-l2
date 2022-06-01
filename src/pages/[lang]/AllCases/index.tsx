c2c6990e75d0930268f5fdd8f7f08b2b156d4f47
HeloiseKatharine <katharineheloise@gmail.com>
2022-06-01 18:03:02 -0300

    Atualização da página

--- src/pages/[lang]/AllCases/index.tsx
+++ src/pages/[lang]/AllCases/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next';
 import { useEffect } from 'react';
 import AllCasesPage from '../../../components/organisms/AllCasesPage';
-
-
-
-//http://localhost:3000/pt/AllCases
+import Menu from '../../../components/organisms/Menu';
+import AllMenu from '../../../components/organisms/AllMenu';
 
 const AllCases: NextPage = () => {
   // make page refresh on top
@@ -20,6 +18,8 @@ const AllCases: NextPage = () => {
 
   return (
     <>
+      {/*<AllMenu />*/}
+      {/*<Menu openMenu={true} closeMenu = {() => (false)}/>*/}
       <AllCasesPage />
     </>
   );
