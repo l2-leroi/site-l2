10ab31b10921988a4489577446064286b1a36eb8
HeloiseKatharine <katharineheloise@gmail.com>
2022-06-10 14:56:18 -0300

    Update DesignMemoriesUp

--- src/components/organisms/DesignMemoriesUp/index.tsx
+++ src/components/organisms/DesignMemoriesUp/index.tsx
@@ -1,36 +1,71 @@
-import { AsideStyled, ContainerStyled, ContentStyled, DivStyled, ParagraphStyled, PhraseStyled, TitleStyled, ButtonClose, ButtonOpen, ImageStyled} from "./styled";
-import React from "react";
+import {ContainerStyled, ContentStyled, DivStyled,TitleStyled, ButtonImages, ImageStyled, ButtonStyled, TechnologiesStyled, TecTitleStyled, ButtonPrototype} from "./styled";
+import React, { useEffect } from "react";
+import {useState} from 'react';
 import i18next from 'i18next';
+import DesignPrototype from "../../molecules/DesignPrototype";
+import DesignImages from "../../molecules/DesignImages";
 
-// import path from 'path';
-// import { DivItemStyled, TextStyled } from "../HeaderMemoriesUp/styled";
-// import Button from "../../atoms/Button";
-// import Spinner from "../../atoms/Spinner";
+function DesignMemoriesUp() {
+  const [button1isShown, button1setIsShown] = useState(true);
+  const [button2isShown, button2setIsShown] = useState(false);
+  const {t}  = i18next;
 
-const DesignMemoriesUp = () => {
-  // const  {t}  = i18next;
+  useEffect(()=> {
+    const btn1 = document.getElementById("btn1");
+    btn1.classList.add("classBtn1");
+    btn1.classList.add("classPadding1");
+    const btn2 = document.getElementById("btn2");
+    btn2.classList.add("classBtn2");
+    btn2.classList.add("classPadding2");
 
-  
+    if(button1isShown == false){
+      btn1.classList.remove("classBtn1");
+      btn2.classList.remove("classPadding2");
+    }
+    if(button2isShown == false){
+      btn2.classList.remove("classBtn2");
+      btn1.classList.remove("classPadding1");
+    }
 
+  },[button1isShown, button2isShown]);
 
-  return (
+  const buttonOpen1 = event => {
+    button1setIsShown(true);
+    button2setIsShown(false);
+  }
+  const buttonOpen2 = event => {
+    button2setIsShown(true);
+    button1setIsShown(false);
+  };
+
+  return ( 
     <ContainerStyled >
       <ContentStyled>
-        <DivStyled>
-          <TitleStyled>Design</TitleStyled>
+
+        <DivStyled> 
+          <TitleStyled>{t('CasesDetail.design')}</TitleStyled>
         </DivStyled>
-        
-        <ButtonOpen>Imagens</ButtonOpen>
-        
-        <ButtonClose>Protótipo</ButtonClose>
 
+        <ButtonStyled>
+          <ButtonPrototype onClick={buttonOpen1} id="btn1"> {t('CasesDetail.prototipo')} </ButtonPrototype>
+          <ButtonImages onClick={buttonOpen2} id="btn2"> {t('CasesDetail.imagens')} </ButtonImages>
+        </ButtonStyled>
+
+        {button1isShown && <DesignPrototype  />}
+        {button2isShown && <DesignImages />}
+       
+        <TecTitleStyled>
+          {t('CasesDetail.tecnologias')}
+        </TecTitleStyled>
+ 
+        <TechnologiesStyled>
+            <ImageStyled src="../images/WebApp.svg" alt="WebApp Logo"/>
+            <ImageStyled src="../images/nodejs-logo.svg" alt="Node.js Logo"/>
+            <ImageStyled src="../images/reactjs-logo.svg" alt="React Logo"/>
+        </TechnologiesStyled>
       
-        
       </ContentStyled>
-
     </ContainerStyled>
-    
-    
   );
 };
 export default DesignMemoriesUp;
--- src/components/organisms/DesignMemoriesUp/styled.ts
+++ src/components/organisms/DesignMemoriesUp/styled.ts
@@ -3,10 +3,13 @@ import { colors } from '../../../styles/colors';
 
 export const ContainerStyled = styled.div`
   color: ${colors.black};
-  background-color: black;
+  background-color: #171b21;
+  padding: 18rem 2.8rem 10rem;
+  display: flex;
+
+
   // clip-path: polygon(0 0, 100% 0, 100% 100%, 0 80%);
   // padding: 10rem 2.8rem 48rem;
-  padding: 10rem 2.8rem 10rem;
 
   // @media (min-width: 2560px) {
   //   clip-path: polygon(0 0, 100% 0, 100% 100%, 0 77%);
@@ -30,17 +33,18 @@ export const ContentStyled = styled.section`
   display: flex;
   flex-direction: column;
   max-width: 1500px;
-  margin: 0 auto;
-
-  
+  //margin: 0 auto;
+  //margin: auto;
+  //height: 1500px;
+  height: 100%;
+  width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export const DivStyled = styled.div`
-  display: flex;
-  align-items: end;
-  margin-bottom: 3.6rem;
+  text-align: center;
   color: White;
-  margin-left: 54rem;
 `;
 
 export const AsideStyled = styled.aside`
@@ -71,33 +75,65 @@ export const ParagraphStyled = styled.p`
   }
 `;
 
+export const TecTitleStyled = styled.h2`
+font-family: 'Neue Machina', sans-serif;
+font-size: 64px;
+line-height: 57.6px;
+text-align: center;
+font-weight: 800;
+color: #F2F3F4;
+letter-spacing: -0.04em;
+
+::selection {
+  background: ${colors.purple};
+  color: ${colors.gray};
+}
+
+// @media (max-width: 930px) {
+//   width: 500px;
+// }
+
+// @media (max-width: 800px) {
+//   width: 278px;
+//   line-height: 28.8px;
+// }
+
+// @media (max-width: 415px) {
+//   width: 264px;
+//   line-height: 28.8px;
+// }
+`;
+
 export const TitleStyled = styled.h2`
+  width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+
   font-family: 'Neue Machina', sans-serif;
-  font-size: 5.4rem;
-  line-height: 57.6px;
+  font-size: 64px;
+  text-align: center;
   font-weight: 800;
-  width: 686px;
+  color: #F2F3F4;
   letter-spacing: -0.04em;
-  
 
   ::selection {
     background: ${colors.purple};
     color: ${colors.gray};
   }
 
-  @media (max-width: 930px) {
-    width: 500px;
-  }
+  // @media (max-width: 930px) {
+  //   width: 500px;
+  // }
 
-  @media (max-width: 800px) {
-    width: 278px;
-    line-height: 28.8px;
-  }
+  // @media (max-width: 800px) {
+  //   width: 278px;
+  //   line-height: 28.8px;
+  // }
 
-  @media (max-width: 415px) {
-    width: 264px;
-    line-height: 28.8px;
-  }
+  // @media (max-width: 415px) {
+  //   width: 264px;
+  //   line-height: 28.8px;
+  // }
 `;
 
 export const PhraseStyled = styled.p`
@@ -119,66 +155,98 @@ export const PhraseStyled = styled.p`
   }
 `;
 
-export const ButtonClose = styled.div`
-  position: absolute;
-  top: 100px;
-  /* right: 45%; */
-  left: 490px;
-  width: 150px;
-  height: 80px;
-  background-color: #000000;
+export const ButtonImages  = styled.button`
+  background-color: #242a33;
+  padding: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
   border-radius: 100px;
   cursor: pointer;
-  display: flex;
-  justify-content: center;
-
   font-family: 'Circular Air Pro';
   font-style: normal;
   font-weight: 400;
   font-size: 24px;
-  line-height: 140%;
-  letter-spacing: -0.04em;
   color: #ffffff;
-  /* padding-left: 130px; */
-  padding-top: 20px;
+  line-height: 140%;
+  border-style: none;
+
+  &.classBtn2{
+    background-color: black;
+    z-index: 2; 
+  }
+  &.classPadding2{
+    padding-left: 80px;
+  }
 `;
 
-export const ButtonOpen = styled.div`
-  position: absolute;
-  top: 100px;
-  left: 490px;
-  width: 300px;
-  height: 80px;
+export const ButtonPrototype  = styled.button`
+  padding: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
   background-color: #242a33;
+  margin-right: -45px;
+  z-index: 2;
   border-radius: 100px;
   cursor: pointer;
-  display: flex;
-  justify-content: center;
-  text-align: center;
-
   font-family: 'Circular Air Pro';
   font-style: normal;
   font-weight: 400;
   font-size: 24px;
-  line-height: 140%;
-  letter-spacing: -0.04em;
   color: #ffffff;
-  padding-left: 130px;
-  padding-top: 20px;
+  line-height: 140%;
+  border-style: none;
+
+  &.classBtn1{
+    background-color: black;
+  }
+
+  &.classPadding1{
+    padding-right: 80px;
+  }
+`;
+
+export const ButtonStyled = styled.div`
+  width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 80px;
+  margin-top: 60px;
+
+  &:first-child {
+    margin-bottom: 8px;
+  }
+
+  &:hover ${ButtonPrototype} 
+  {
+    background-color: #242a33;
+  }
+
+  ${ButtonPrototype}:hover + ${ButtonImages}
+  {
+    background-color: #242a33;
+  }
+
+  ${ButtonPrototype}:hover 
+  {
+    background-color: black;
+  }
+
+  ${ButtonImages}:hover{
+    background-color: black;
+  }
 `;
 
 export const ImageStyled = styled.img`
-  position: absolute;
-  /* margin-left: -212px;
-  height: 105%;
-  margin-top: -11px; */
-  width: 675px;
-  height: 938px;
+  width: 150px;
+  height: 150px;
   left: 0px;
-  /* top: 2400px; */
 
   @media (max-width: 800px) {
-    max-width: 164px;
+    max-width: 150px;
   }
 
   @media (max-width: 576px) {
@@ -194,5 +262,10 @@ border-radius: 24px;
 } 
 `;
 
-
-
+export const TechnologiesStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 60px;
+  grid-gap: 80px;
+`;
