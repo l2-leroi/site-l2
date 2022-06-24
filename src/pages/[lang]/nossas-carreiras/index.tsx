1e6641435a72ebdf828cde7a2938d0b0f9d2ead5
Juliana-flores <floresjuliana@live.com>
2022-06-24 15:11:05 -0300

    alterações para montagem dos components na página

--- src/pages/[lang]/nossas-carreiras/index.tsx
+++ src/pages/[lang]/nossas-carreiras/index.tsx
@@ -1,31 +1,45 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import { useEffect } from 'react';
-import { GlobalStyle } from '../../../styles/global';
-import SectionTitle from '../../../components/organisms/sectionTitle';
-import Registration from '../../../components/organisms/registration';
-import TraineeImg from '../../../components/organisms/trainee-img';
-import TextCarroussel from '../../../components/organisms/text-carroussel';
+
+// import TraineeImg from '../../../components/organisms/trainee-img';
+// import TextCarroussel from '../../../components/organisms/text-carroussel';
+import HeaderOurCareer from '../../../components/organisms/HeaderOurCareer';
 
 const NossasCarreiras: NextPage = () => {
-
   useEffect(() => {
     if (history.scrollRestoration) {
       history.scrollRestoration = 'manual';
     } else {
-      window.onbeforeunload = function () {
+      window.onbeforeunload = () => {
         window.scrollTo(0, 0);
       };
     }
   });
+
+  const startDate = '20/07/2022';
+  const endDate = '24/07/2022';
+  const title = 'Inscrições até:';
+  const subTitle = 'Resultado em:';
+  const text =
+    'Faça sua candidatura agora e receba resposta primeiro. Fique tranquila, você pode atualizar seus dados depois.';
+  const textEnd = 'Aguarde o resultado. Iremos encaminhá-lo a você por e-mail.';
+  const id = 1;
   return (
     <>
-    <SectionTitle />
-    <Registration text='Faça sua candidatura agora e receba resposta primeiro. Fique tranquila, você pode atualizar seus dados depois.'/>
-    <TraineeImg />
-    <TextCarroussel />
+      <HeaderOurCareer
+        startDate={startDate}
+        endDate={endDate}
+        title={title}
+        subTitle={subTitle}
+        text={text}
+        textEnd={textEnd}
+        id={id}
+      />
 
+      {/* <TraineeImg /> */}
+      {/* <TextCarroussel /> */}
     </>
-  );       
-}
+  );
+};
 
 export default NossasCarreiras;
