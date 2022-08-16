55c5a25a3bd0bc7838837fb91827a12799e35b26
HeloiseKatharine <katharineheloise@gmail.com>
2022-08-16 13:51:47 -0300

    Criação da página "pessoas"

---
+++ src/components/organisms/PeoplePage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import i18next from 'i18next';
+import { Typography } from '../../../styles/typography';
+
+export default function PeoplePage() {
+  /* return (); */
+}
---
+++ src/pages/[lang]/pessoas/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import LineAnimation from '../../../components/atoms/LineAnimation';
+import PeoplePage from '../../../components/organisms/PeoplePage';
+import { colors } from '../../../styles/colors';
+
+const Pessoas: NextPage = () => {
+  // make page refresh on top
+  useEffect(() => {
+    if (history.scrollRestoration) {
+      history.scrollRestoration = 'manual';
+    } else {
+      window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+      };
+    }
+  });
+
+  return (
+    <>
+      <PeoplePage />
+      <LineAnimation
+        lineBg={colors.purple}
+        secondaryBg={colors.black}
+        hasOutSourcing={false}
+        hasSpinner={false}
+      />
+    </>
+  );
+};
+
+export default Pessoas;
