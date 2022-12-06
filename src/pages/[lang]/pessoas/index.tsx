4fcbbcf666afd84e30557b7d65353be79b69cd47
HeloiseKatharine <katharineheloise@gmail.com>
2022-12-06 19:05:33 -0300

    Ajustes na página

--- src/pages/[lang]/pessoas/index.tsx
+++ src/pages/[lang]/pessoas/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next';
 import { useEffect } from 'react';
 import LineAnimation from '../../../components/atoms/LineAnimation';
+import Footer from '../../../components/organisms/Footer';
+import PeoplePage from '../../../components/organisms/PeoplePage';
 import { colors } from '../../../styles/colors';
 
 const Pessoas: NextPage = () => {
   // make page refresh on top
   useEffect(() => {
+    const nav = document.querySelector('.nav');
+    nav?.classList.add('black');
+
     if (history.scrollRestoration) {
       history.scrollRestoration = 'manual';
     } else {
@@ -13,17 +18,18 @@ const Pessoas: NextPage = () => {
         window.scrollTo(0, 0);
       };
     }
-  });
+  }, []);
 
   return (
     <>
-      {/* <PeoplePage /> */}
+      <PeoplePage />
       <LineAnimation
-        topBgColor={colors.purple}
-        bottomBgColor={colors.black}
+        topBgColor={colors.black}
+        bottomBgColor={colors.purple}
         hasOutSourcing={false}
         hasSpinner={false}
       />
+      <Footer />
     </>
   );
 };
